a8d344d3e72a7d83a90cd7d6d0e9c44c20f85352
Giacomo Franceschetto <65191292+GiacomoFrn@users.noreply.github.com>
2025-04-28 16:23:16 +0200

    Update main.py

--- arxiv_update_bot/main.py
+++ arxiv_update_bot/main.py
@@ -125,7 +125,7 @@ def get_articles(category: str, buzzwords: List[str]) -> List:
         for author in entry.authors[0]['name'].split(', '):
             normalized_author = normalize_name(author)
             if author == "Jakob G\"unther":
-                print(is_name_match(normalized_author, "Jakob Gunther")
+                print(is_name_match(normalized_author, "Jakob Gunther"))
             if any(is_name_match(normalized_author, watch_name) for watch_name in normalized_watchlist) :#or any(buzzword in entry.title.lower() for buzzword in buzzwords):
                 res.append(entry)
                 break  # Avoid adding the same entry multiple times
