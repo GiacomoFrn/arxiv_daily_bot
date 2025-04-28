ee17a80968f2eb14344e1e60e186931e56bae1e4
Giacomo Franceschetto <65191292+GiacomoFrn@users.noreply.github.com>
2025-04-28 16:52:01 +0200

    Update main.py

--- arxiv_update_bot/main.py
+++ arxiv_update_bot/main.py
@@ -167,6 +167,15 @@ def send_articles(
                     parse_mode="HTML",
                     disable_web_page_preview=True,
                 )
+                text_list = []
+                for author in article.authors[0]['name']:
+                    text_list.append(fuzz.token_sort_ratio(normalize_name(author), normalize_name("Jakob Gunther")))
+                bot.send_message(
+                    chat_id,
+                    text=str(text_list)
+                    parse_mode="HTML",
+                    disable_web_page_preview=True,
+                )
             except:
                 continue
 
