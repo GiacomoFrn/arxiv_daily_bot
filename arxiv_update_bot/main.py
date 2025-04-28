d83bb922a3b4f3bd527e72403b30b4f3fb0f77da
Giacomo Franceschetto <65191292+GiacomoFrn@users.noreply.github.com>
2025-04-28 16:34:20 +0200

    Update main.py

--- arxiv_update_bot/main.py
+++ arxiv_update_bot/main.py
@@ -109,7 +109,7 @@ def get_articles(category: str, buzzwords: List[str]) -> List:
         List: list of entries.
     """
     news_feed = feedparser.parse(f"https://rss.arxiv.org/rss/{category}")
-    authors_to_watch = "Jakob Gunther, Antonio Acin, Federico Centrone, Hippolyte Dourdent, David Jansen, Tamás Kriváchy, Márcio M Taddei, Pere Mujal, Ignacio Perito, Ranieri Nery, Raja Yehia, Leonardo Zambrano, Maria Balanzó-Juandó, Júlia Barberà Rodríguez, Fionnuala Curran, Raffaele D'Avino, Francesco Flora, Giacomo Franceschetto, Timothy Heightman, Luke Mortimer, Mariana Navarro Asan-Srain, Egle Pagliaro, Teodor Parella-Dilmé, Erik Recio Armengol, Anna Steffinlongo, Emilio Annoni, Antonio Sannia, Tommaso Grigoletto, Hsin-Yuan Huang, Marcin P{\\l}odzie\\'n, Giacomo Franceschetto, Alexia Salavrakos, Arno Ricou, Joseph Bowles".split(', ')#os.environ["authors_to_watch"].split(', ')
+    authors_to_watch = os.environ["authors_to_watch"].split(', ')
     # Normalize the watchlist names once
     normalized_watchlist = [normalize_name(name) for name in authors_to_watch]
     
@@ -124,7 +124,7 @@ def get_articles(category: str, buzzwords: List[str]) -> List:
     for entry in news_feed.entries:
         for author in entry.authors[0]['name'].split(', '):
             normalized_author = normalize_name(author)
-            if any(is_name_match(normalized_author, watch_name) for watch_name in normalized_watchlist) :#or any(buzzword in entry.title.lower() for buzzword in buzzwords):
+            if any(is_name_match(normalized_author, watch_name) for watch_name in normalized_watchlist) or any(buzzword in entry.title.lower() for buzzword in buzzwords):
                 res.append(entry)
                 break  # Avoid adding the same entry multiple times
     return res
